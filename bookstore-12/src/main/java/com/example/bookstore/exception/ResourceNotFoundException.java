a4b3ff2f6b03118130b37746fdb61f0e08ef3f51
snow369 <115411589+snow369@users.noreply.github.com>
2024-08-28 21:46:10 +0530

    Delete bookstore-12 directory

--- bookstore-12/src/main/java/com/example/bookstore/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstore.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
